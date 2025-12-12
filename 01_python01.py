258e46207f17929b92ae913b106a23fdaaea3c5c
kathiriyameet99 <kathiriyameet389@gmail.com>
2025-12-12 11:51:10 +0530

    Add sum calculator

--- 01_python01.py
+++ 01_python01.py
@@ -1,1 +1,11 @@
 print("Hello World")
+
+#
+
+a = int(input("Enter the value: "))
+b = int(input("Enter the value: "))
+
+print(f"The sum a and b is: {a + b}")
+print(f"The sum a and b is: {a - b}")
+print(f"The sum a and b is: {a * b}")
+print(f"The sum a and b is: {a / b}")
