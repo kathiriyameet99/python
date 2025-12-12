fa5db9ce416a651ce66e2b791c8f6883c5965e69
kathiriyameet99 <kathiriyameet389@gmail.com>
2025-12-12 12:25:53 +0530

    Add the word print 10 time

--- 01_python01.py
+++ 01_python01.py
@@ -1,12 +1,16 @@
-print("Hello World")
+# print("Hello World")
 
-for i in range(1, 11):
-    print("Hello World")
+# for i in range(1, 11):
+#     print("Hello World")
 
-a = int(input("Enter the value: "))
-b = int(input("Enter the value: "))
+# a = int(input("Enter the value: "))
+# b = int(input("Enter the value: "))
 
-print(f"The sum a and b is: {a + b}")
-print(f"The sum a and b is: {a - b}")
-print(f"The sum a and b is: {a * b}")
-print(f"The sum a and b is: {a / b}")
+# print(f"The sum a and b is: {a + b}")
+# print(f"The sum a and b is: {a - b}")
+# print(f"The sum a and b is: {a * b}")
+# print(f"The sum a and b is: {a / b}")
+
+#Assignment Question : Print your name 10 times using * symbol and it should be next line
+
+print(("Hello Guys!"+"\n")*10)
